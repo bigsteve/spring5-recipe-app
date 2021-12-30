ce00fcb64f94922837f27d66d11c28eed393b656
stefan <usstefanmiller@yahoo.com>
2021-12-30 17:01:16 -0600

    refactored packages and clesses

--- src/main/java/learn/thymeleaf/services/IngredientService.java
+++ src/main/java/learn/thymeleaf/services/IngredientService.java
@@ -9,4 +9,5 @@ import learn.thymeleaf.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
-}
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);}
